dc76ff2364e45a9ad50b7256b8e54e771d38ffbc
Harsh-Makadiya <108142988+Harsh-Makadiya@users.noreply.github.com>
2023-02-16 12:51:26 +0530

    f

---
+++ models/campground.js
@@ -0,0 +1,64 @@
+const { ref } = require('joi');
+const mongoose = require('mongoose');
+const review = require('./review');
+const Schema = mongoose.Schema;
+const imageSchema =  new Schema ({
+    url: String,
+    filename: String
+})
+
+imageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
+const opts = { toJSON: { virtuals: true } };
+
+const campgroundSchema = new Schema({
+    title: String,
+    images: [
+        imageSchema
+    ],
+    geometry:{
+        type:{
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates:{
+            type: [Number],
+            required: true
+        }
+    },
+    createdDate : String,
+    price:  Number,
+    description: String,
+    location: String,
+    author:{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'review'
+        }
+    ]
+}, opts)
+campgroundSchema.virtual('properties.PopUpMarkup').get(function(){
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong><p>
+     ${this.description.substring(0,25)}...   
+    </p>`
+})
+
+campgroundSchema.post('findOneAndDelete', async (doc)=>{
+    if(doc){
+        await review.deleteMany({
+            _id:{
+                $in : doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('campground', campgroundSchema);
